3a4c5200f5366496c0d0efa9ca46f82858810640
Nikita Kolmogorov <backmeupplz@gmail.com>
2020-05-12 16:18:52 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  v-app(:dark='$store.state.dark' :class='$store.state.dark ? "grey darken-4" : "grey lighten-4"')
+    cookie-law(theme='blood-orange'
+    :buttonText='$t("cookieButton")'
+    :message='$t("cookieMessage")')
+    Navbar
+    Snackbar
+    v-content
+      router-view
+</template>
+
+<script lang="ts">
+import Navbar from './components/Navbar.vue'
+import Snackbar from './components/Snackbar.vue'
+import * as store from './plugins/store'
+import CookieLaw from 'vue-cookie-law'
+
+export default {
+  components: { Navbar, Snackbar, CookieLaw },
+  created() {
+    ;(this as any).$vuetify.theme.dark = store.dark()
+  },
+}
+</script>
---
+++ src/plugins/router.ts
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../views/Home.vue'
+import NotFound from '../views/NotFound.vue'
+import { store } from './store'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '*',
+      name: 'notFound',
+      component: NotFound,
+    },
+  ],
+})
+
+router.beforeEach((to, _, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth)
+  const user = store.state.user
+
+  if (requiresAuth && !user) {
+    next('/')
+  } else {
+    if (to.path === '/' && user) {
+      next('/cabinet')
+    } else {
+      next()
+    }
+  }
+})
+
+export default router
