5bee68442906b0b7917555d984e18609fe15763b
backmeupplz <backmeupplz@gmail.com>
2020-05-14 15:12:29 -0700

    add searcn to admin

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -3,6 +3,7 @@
     .headline {{$t('admin.title')}}
     .body-1 {{$t('admin.description')}}
     PasswordInput
+    SearchInput
     LocalizationCards(admin)
 </template>
 
@@ -12,12 +13,14 @@ import * as store from '../plugins/store'
 import Component from 'vue-class-component'
 import { i18n } from '../plugins/i18n'
 import PasswordInput from '../components/PasswordInput.vue'
+import SearchInput from '../components/SearchInput.vue'
 import LocalizationCards from '../components/LocalizationCards.vue'
 
 @Component({
   components: {
     PasswordInput,
     LocalizationCards,
+    SearchInput,
   },
 })
 export default class Admin extends Vue {}
